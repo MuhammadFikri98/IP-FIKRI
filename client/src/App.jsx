675c5dc550822b0f8f50d21ce88d377fca7a9fd4
fikri <fikri.algabel99@gmail.com>
2025-04-30 01:11:37 +0700

    feat: client for react

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+function App() {
+
+
+  return (
+    <>
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>  
+    </>
+  )
+}
+
+export default App
